2740905a0732b98c6647dda2b47dbb9385973f3d
Guest User <Guest@Mac110-Selesnya.local>
2013-01-21 12:35:31 -0500

    Turning in program 0 part A

---
+++ cs132_program_0_smucha/cs132_program_0_smucha/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  cs132_program_0_smucha
+//
+//  Created by Guest User on 1/21/13.
+//  Copyright (c) 2013 Stacey Mucha. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
