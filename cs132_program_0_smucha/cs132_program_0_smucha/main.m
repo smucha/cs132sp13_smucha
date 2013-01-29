6df066df13fa80bc723990dd99d480321d30f630
Guest User <Guest@Mac110-Selesnya.local>
2013-01-29 18:40:50 -0500

    Added the code for creating and filling an array

--- cs132_program_0_smucha/cs132_program_0_smucha/main.m
+++ cs132_program_0_smucha/cs132_program_0_smucha/main.m
@@ -14,7 +14,27 @@ int main(void)
 {
     
     @autoreleasepool {
-    
+        
+        
+        NSMutableArray* words = nil;
+        words = [[NSMutableArray alloc] init];
+        
+        NSLog(@"words, Uninitialized: (null)");
+        
+        NSLog(@"words, Initialized: %@", words);
+        
+        [words addObject:(@", ")];
+        NSLog(@"words, one added: %@", words);
+        
+        [words addObject:(@"Hello")];
+        NSLog(@"words, two added: %@", words);
+        
+        [words addObject:(@"world")];
+        NSLog(@"words, all added: %@", words);
+        
+        [words sortUsingSelector: @selector(localizedCaseInsensitiveCompare:)];
+        NSLog(@"words, sorted using localizedCaseInsensitiveCompare: %@", words);
+        
     }
     return EXIT_SUCCESS;
 }
