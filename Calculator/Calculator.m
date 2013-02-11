c8a805a2127352452e2ca6c1555993d0385b6f4f
Stacey Mucha <smucha@Mac110-Selesnya.local>
2013-02-10 22:46:12 -0500

    -pressKey: code isn't working. Still unable to clear GUI calculator.

--- Calculator/Calculator.m
+++ Calculator/Calculator.m
@@ -16,11 +16,22 @@
 
 -(void)pressKey:(char)theKey
 {
-    int old;
-	old = [self numberOnScreen];
-	int newDigit;
-	newDigit = theKey - '0';
-	[self setNumberOnScreen: 10 * old + newDigit];
+    if('0' <= theKey <= '9')
+    {
+        int old;
+        old = [self numberOnScreen];
+        int newDigit;
+        newDigit = theKey - '0';
+        [self setNumberOnScreen: 10 * old + newDigit];
+    } else {
+        if( theKey == 'C')
+        {
+            numberOnScreen = '0';
+        } else {
+            NSLog(@"Uncovered argument '%c' in %@ message received by object at %p (%@)", theKey, NSStringFromSelector(_cmd), self, self);
+        }
+    
+    }
 }
 
 -(NSString*) description
