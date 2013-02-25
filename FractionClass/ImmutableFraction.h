ba439ce17ee570c263a179697e7dda303ed9894f
Stacey Mucha <smucha@Mac110-Selesnya.local>
2013-02-24 21:39:31 -0500

    P2a setup

---
+++ FractionClass/ImmutableFraction.h
@@ -0,0 +1,13 @@
+//
+//  ImmutableFraction.h
+//  
+//
+//  Created by Stacey Mucha on 2/24/13.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ImmutableFraction : NSObject
+
+@end
---
+++ FractionClass/ImmutableFraction.m
@@ -0,0 +1,13 @@
+//
+//  ImmutableFraction.m
+//  
+//
+//  Created by Stacey Mucha on 2/24/13.
+//
+//
+
+#import "ImmutableFraction.h"
+
+@implementation ImmutableFraction
+
+@end
