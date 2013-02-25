968f8a723f2c8bc9fc3e1a4f579eb6a1d3c7cc8b
Stacey Mucha <smucha@Mac110-Selesnya.local>
2013-02-24 23:14:48 -0500

    Task FracFracImps
    Created stub implementations for all of the fraction with fraction and fraction from fraction methods.

--- FractionClass/ImmutableFraction.m
+++ FractionClass/ImmutableFraction.m
@@ -16,12 +16,52 @@
 
 - (id)init
 {
-    printf("init method run");
+    printf("init method ran");
 }
 
 - (id)description
 {
-    printf("description method run");
+    printf("description method ran");
+}
+
+- (Fraction*)negative
+{
+    printf("negative method ran");
+}
+
+- (Fraction*)reciprocal
+{
+    printf("reciprocal method ran");
+}
+
+- (Fraction*)sumWith:(Fraction*) sum
+{
+    printf("sum with method ran");
+}
+
+- (Fraction*)subtractFrom:(Fraction*) subtract
+{
+    printf("subtract from method ran");
+}
+
+- (Fraction*)minus:(Fraction*) m
+{
+    printf("minus method ran");
+}
+
+- (Fraction*)multiplyBy:(Fraction*) multiply
+{
+    printf("multiply by method ran");
+}
+
+- (Fraction*)divideBy:(Fraction*) divide
+{
+    printf("divide by method ran");
+}
+
+- (Fraction*)divideInto:(Fraction*) into
+{
+    printf("divide into method ran");
 }
 
 @end
