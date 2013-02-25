6d28a1775dc09e9b362a345ac4e1c85c34210361
Stacey Mucha <smucha@Mac110-Selesnya.local>
2013-02-24 22:03:43 -0500

    Task ROProps
    Created two readonly integer properties

--- FractionClass/ImmutableFraction.h
+++ FractionClass/ImmutableFraction.h
@@ -1,6 +1,6 @@
 //
 //  ImmutableFraction.h
-//  
+//
 //
 //  Created by Stacey Mucha on 2/24/13.
 //
@@ -9,5 +9,9 @@
 #import <Cocoa/Cocoa.h>
 
 @interface ImmutableFraction : NSObject
+
+@property (readonly,assign) int numerator;
+
+@property (readonly,assign) int denominator;
 
 @end
--- FractionClass/ImmutableFraction.m
+++ FractionClass/ImmutableFraction.m
@@ -10,4 +10,8 @@
 
 @implementation ImmutableFraction
 
+@synthesize numerator;
+
+@synthesize denominator;
+
 @end
