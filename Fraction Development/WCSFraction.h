571106d186d1e1cea0b7fbe262e3d95cd3f8d9de
Stacey Mucha <smucha@Mac110-Selesnya.local>
2013-03-22 10:41:52 -0400

    Created a Objective-C class named WCSFraction in the Fraction Code group

---
+++ Fraction Development/WCSFraction.h
@@ -0,0 +1,13 @@
+//
+//  WCSFraction.h
+//  Fraction Development
+//
+//  Created by Stacey Mucha on 3/22/13.
+//  Copyright (c) 2013 Wells CS132: Object Oriented Programming. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WCSFraction : NSObject
+
+@end
---
+++ Fraction Development/WCSFraction.m
@@ -0,0 +1,13 @@
+//
+//  WCSFraction.m
+//  Fraction Development
+//
+//  Created by Stacey Mucha on 3/22/13.
+//  Copyright (c) 2013 Wells CS132: Object Oriented Programming. All rights reserved.
+//
+
+#import "WCSFraction.h"
+
+@implementation WCSFraction
+
+@end
