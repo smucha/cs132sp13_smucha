4d14e32a4b34575f07a8cf9754bda8240193d31c
Stacey Mucha <smucha@Mac110-Selesnya.local>
2013-03-27 18:59:02 -0400

    Created the WCSMutableFraction class.

---
+++ Fraction Development/WCSMutableFraction.m
@@ -0,0 +1,13 @@
+//
+//  WCSMutableFraction.m
+//  Fraction Development
+//
+//  Created by Stacey Mucha on 3/27/13.
+//  Copyright (c) 2013 Wells CS132: Object Oriented Programming. All rights reserved.
+//
+
+#import "WCSMutableFraction.h"
+
+@implementation WCSMutableFraction
+
+@end
