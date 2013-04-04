dcc89d2fd28969af0fafd5af934ae41834745753
Stacey Mucha <smucha@Mac110-Selesnya.local>
2013-04-04 16:12:14 -0400

    Created the NSMutableCopying protocol.

---
+++ Fraction Development/NSMutableCopying.h
@@ -0,0 +1,13 @@
+//
+//  NSMutableCopying.h
+//  Fraction Development
+//
+//  Created by Stacey Mucha on 4/4/13.
+//  Copyright (c) 2013 Wells CS132: Object Oriented Programming. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol NSMutableCopying <NSObject>
+
+@end
