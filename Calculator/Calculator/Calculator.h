1ce39163456761ed8494212287ff11399bad52a6
Stacey Mucha <smucha@Mac110-Selesnya-2.local>
2013-05-03 15:24:47 -0400

    Ready to turn in Program 3 Part B

---
+++ Calculator/Calculator/Calculator.h
@@ -0,0 +1,44 @@
+#import <Foundation/Foundation.h>
+#import "WCSFraction.h"
+#import "WCSMutableFraction.h"
+
+typedef enum {
+	WCSPartTop,
+	WCSPartBottom
+} WCSFractionPart;
+
+@interface Calculator : NSObject
+
+@property (strong, readwrite)  WCSMutableFraction* numberOnScreen;
+
+@property (strong, readwrite) WCSFraction* numberAccumulated;
+
+@property (assign) char operationPending;
+
+@property (assign) WCSFractionPart fractional;
+
+-(void) pressKey:(char) theKey;
+
+-(void)appendDigit:(char) theKey;
+
+-(void)clearScreen:(char) theKey;
+
+-(void) registerArithmetic:(char)theOperator;
+
+-(void) computeAndDisplayResult;
+
+-(void) clearAccumulator;
+
+-(void) clearOperation;
+
+@end
+
+BOOL isAdigit(char pressKey);
+
+BOOL isClearScreenKey(char pressKey);
+
+BOOL isClearAllKey(char pressKey);
+
+BOOL isResultKey(char pressKey);
+
+BOOL isArithmeticAllKey(char pressKey);
