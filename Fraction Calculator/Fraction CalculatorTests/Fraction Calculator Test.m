5b5a8515f2c41b9d53ffd6fd2528731958405ac5
Stacey Mucha <smucha@Mac110-Selesnya-2.local>
2013-05-04 14:31:16 -0400

    Finally fixed entire project. Larry helped rebuild the entire project. Can now commit without any problems.

---
+++ Fraction Calculator/Fraction CalculatorTests/Fraction Calculator Test.m
@@ -0,0 +1,38 @@
+//
+//  Fraction Calculator - Fraction Calculator Test.m
+//  Copyright 2013 Stacey Mucha. All rights reserved.
+//
+//  Created by: Stacey Mucha
+//
+
+    // Class under test
+#import "<#Class Header#>"
+
+    // Collaborators
+
+    // Test support
+#import <SenTestingKit/SenTestingKit.h>
+
+// Uncomment the next two lines to use OCHamcrest for test assertions:
+//#define HC_SHORTHAND
+//#import <OCHamcrest/OCHamcrest.h>
+
+// Uncomment the next two lines to use OCMockito for mock objects:
+//#define MOCKITO_SHORTHAND
+//#import <OCMockito/OCMockito.h>
+
+
+@interface Fraction_Calculator_Test : SenTestCase
+@end
+
+@implementation Fraction_Calculator_Test
+{
+    // test fixture ivars go here
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Fraction_Calculator_Test");
+}
+
+@end
